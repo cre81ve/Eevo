7b065dc432bb7ef9e400bb394ca3295e39e82dbf
Paul Lo <plorentus@yahoo.com>
2014-10-12 18:12:25 -0700

    Added LoggedInViewController

--- Eevo/AppDelegate.swift
+++ Eevo/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        Parse.setApplicationId("Duz271CygXpCc8IsoUyNybDvGp4By1LKmb3nlzfC", clientKey: "FmV10FvtNZZysKXs4m5sBMKIiOLUAEi3iXR4DGU8")
         return true
     }
 
